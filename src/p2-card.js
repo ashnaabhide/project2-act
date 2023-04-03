8a88bfd6cb0a19d4aafc4aea7a2ec9b62dfc8e99
ashnaabhide <32545625+ashnaabhide@users.noreply.github.com>
2023-04-03 08:45:45 -0400

    restructuring of html

--- src/p2-card.js
+++ src/p2-card.js
@@ -15,10 +15,18 @@ export class P2Card extends LitElement {
         type: String,
         reflect: true
       }, 
-      detail: {
+      icon: {type: String},
+      badgeDescription: {
         type: String,
       }, 
       opened: {type: Boolean, reflect: true},
+      link: {type: String}, 
+      author: {type: String}, 
+      authorImg: {type: String}, 
+      time: {type: String},
+      steps: {type: Array}, 
+      stepDescription: {type: String}
+
     }
   }
 
@@ -96,10 +104,17 @@ export class P2Card extends LitElement {
 
   constructor() {
     super();
-    this.name="";
-    this.details="";
-  
-    this.opened = true;
+    this.name="test";
+    this.icon="www.google.com";
+    this.badgeDescription="something";
+    this.opened=false;
+    this.link="https://drive.google.com/drive/u/0/my-drive";
+    this.author="john smith";
+    this.authorImg="https://stock.adobe.com/images/happy-smiley-face-or-emoticon-line-art-icon-for-apps-and-websites/112439016";
+    this.time="4.0 hours";
+    this.steps=[ ];
+
+    
   }
 
   toggleEvent(e) {
@@ -152,135 +167,35 @@ export class P2Card extends LitElement {
 <!--<img class="image" src="$(meme)"/> -->
    
     <!--badge 1-->
-  <div class="header">
-    <div class="title">Amazon Cognito
+  <div class="badge">
+    <div class="name">${this.name}
       <div class="img">
-        <img src=https://badgesapp.psu.edu/uploads/badge/image/623/Cognito.png 
-            >
+        <img src=${this.icon}>
       </div>
  <details class="details" .open="${this.opened}" @toggle="${this.toggleEvent}">
  
       <div>
-        <p class="badgedescription">
-          <slot>Learn the basics of how Amazon Cognito works, and how you can use it to create User Sign In, Sign In, Access Control, User Pools, and Identity Pools
- 
-https://docs.aws.amazon.com/cognito/latest/developerguide/tutorials.html
- 
- 
- 
- 
-Badge Creator: Joshua Hantman's user image Joshua Hantman
-Approximate time to complete: 4.0 hours</slot>
+        <p class="badgeDescription">
+          ${this.badgeDescription}
+  
       </p>
-     
+      <div class="link">
+      <a href=${this.link}>${this.link}</a>
+
       </div>
+      <div>
+        ${this.author} 
+        <img src=${this.authorImg}>
+      </div>
+      ${this.time}
+      ${this.steps}
   </details>
       </div>
-  
+    </div>
     </div>
     </div>
   </div>
  
- 
-<!--number 2-->
-  <div class="wrapper">
-   
-<!--<img class="image" src="$(meme)"/> -->
-  <div class="header">
-    <div class="title">Amazon I AM
-      <div class="img">
-        <img src=https://badgesapp.psu.edu/uploads/badge/image/618/AWS-IAM-Exploitation.png.png 
-            >
-      </div>
- <details class="details" .open="${this.opened}" @toggle="${this.toggleEvent}">
-     <p class="badgedescription">
-          <slot>Create a I AM account with MFA enabled and Admin permissions
- 
-Badge Creator: Joshua Hantman's user image Joshua Hantman
-Approximate time to complete: 2.0 hours</slot>
-  </details>
-      </div>
-  
-    </div>
-    </div>
- 
-<!--number 3-->
-  <div class="wrapper">
-   
-<!--<img class="image" src="$(meme)"/> -->
-  <div class="header">
-    <div class="title">Signing up for AWS Web Services
-      <div class="img">
-        <img src=https://badgesapp.psu.edu/uploads/badge/image/620/amazon-web-services_vxmp.png
-            >
-      </div>
- <details class="details" .open="${this.opened}" @toggle="${this.toggleEvent}">
-      <p class="badgedescription">
-        <slot>Learn how to create a root account with AWS Cloud Services and manage your AWS Console.
- 
- Badge Creator: Joshua Hantman's user image Joshua Hantman
- Approximate time to complete: 2.0 hours</slot>
-</p>
-          
-  
-  </details>
-      </div>
-  
-    </div>
-    </div>
- 
-  
-  
-  
-  <!--number 4-->
-
-  <div class="wrapper">
-   
-<!--<img class="image" src="$(meme)"/> -->
-  <div class="header">
-    <div class="title">AWS- LAMBDA Phase 1
-      <div class="img">
-        <img src=https://badgesapp.psu.edu/uploads/badge/image/619/1-yaTKFg6K8tTPXJtBUZ0_lw.png
-          >
-      </div>
- <details class="details" .open="${this.opened}" @toggle="${this.toggleEvent}">
-     <p class="badgedescription">
-          <slot>AWS Lambda is an event-driven, serverless computing platform provided by Amazon as a part of Amazon Web Services. It is a computing service that runs code in response to events and automatically manages the computing resources required by that code.
-AWS Lambda Info Link</slot>
-</p>
-  </details>
-      </div>
-  
-    </div>
-    </div>
-  <!--number 5-->
-
-  <div class="wrapper">
-   
-<!--<img class="image" src="$(meme)"/> -->
-  <div class="header">
-    <div class="title">Amazon S3 Phase 1
-      <div class="img">
-        <img src=https://badgesapp.psu.edu/uploads/badge/image/621/1_B9CIOrxdROHvtdmouQA1_A.png
-            >
-      </div>
- <details class="details" .open="${this.opened}" @toggle="${this.toggleEvent}">
-     
-      <div>
-        <p class="badgedescription">
-          <slot>Amazon S3 is all about storage. Learning how to use it and add some static files to it are all part of this badge. S3 stands for Amazon Simple Storage Service.
- 
-Getting started look through this page Amazon S3 Documentation</slot>
-      </p>
-     
-      </div>
-     
-  </details>
-      </div>
-  
-    </div>
-    </div>
-
     `;
   }
 }
